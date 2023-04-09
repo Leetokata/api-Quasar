adb7c8fd9381dec8479fe89d765fd9f5ed6ca0d2
Luis Davalillo <39009600+Leetokata@users.noreply.github.com>
2023-04-09 13:29:37 -0400

    Update web.php
    cambios en la ruta web de laravel

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('{path}', fn() => view('app') )->where('path', '(.*)');
+ Route::get('{path}', fn() => view('app') )->where('path', '(.*)');
